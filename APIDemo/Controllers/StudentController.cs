38742b5deff81d2180303aea3fe75a54fa415b10
ijaydevpatel <k787jaydev@gmail.com>
2024-05-22 23:12:40 +0530

    initial and all

---
+++ APIDemo/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        static List<Student> students = new List<Student>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Student> Get(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public ActionResult<Student> Post([FromBody] Student value)
+        {
+            students.Add(value);
+            return CreatedAtAction(nameof(Get), new { id = value.Id }, value);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Student value)
+        {
+            int index = students.FindIndex(s => s.Id == id);
+            if (index == -1)
+            {
+                return NotFound();
+            }
+            students[index] = value;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            students.Remove(student);
+            return NoContent();
+        }
+    }
+}
